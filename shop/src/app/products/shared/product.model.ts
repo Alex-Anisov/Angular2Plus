b174f9705d0ec24ed1b72e77892d2182f4ccac17
Oleksandr Anisov <oleksandr_anisov@epam.com>
2017-11-13 17:51:51 +0200

    Fix card -> cart Use public constructor parameters to write less code.

--- shop/src/app/products/shared/product.model.ts
+++ shop/src/app/products/shared/product.model.ts
@@ -3,26 +3,11 @@ import {Ingridients} from './ingridients'
 
 export class Product{
 
-    name: string;
-    description: string;
-    price: number;
-    category: Categories;
-    isAvailable: boolean;
-    ingridients: Array<Ingridients>;
-
     constructor(
-        name: string,
-        description: string,
-        price: number,
-        category: Categories,
-        isAvailable: boolean,
-        ingridients: Array<Ingridients>)
-        {
-            this.name = name;
-            this.description = description;
-            this.price = price;
-            this.category = category;
-            this.isAvailable = isAvailable;
-            this.ingridients = ingridients;
-    }
+        public name: string,
+        public description: string,
+        public price: number,
+        public category: Categories,
+        public isAvailable: boolean,
+        public ingridients: Array<Ingridients>){}
 }
